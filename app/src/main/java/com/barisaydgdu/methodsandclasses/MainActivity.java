0e8def45e2d36b01c2c02023e36485c681a6b599
Barış AYDOĞDU <barisaydogdu76@gmail.com>
2023-11-05 00:10:24 +0300

    Java Temelleri Bitti.

--- app/src/main/java/com/barisaydgdu/methodsandclasses/MainActivity.java
+++ app/src/main/java/com/barisaydgdu/methodsandclasses/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String username;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +20,36 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(math(4,9));
         System.out.println(newMethod("Baris"));
 
+        username = "";
+
+        makeMusicians();
+
+        makeSimpsons();
+
+
+    }
+
+    public void makeSimpsons (){
+        Simpsons homer = new Simpsons("Homer",50,"Nuclear");
+        System.out.println(homer.getName());
+        homer.setName("Homer Simpson");
+        System.out.println(homer.getName());
+    }
+
+    public void makeMusicians(){
+
+        //instance
+
+        Musicians james = new Musicians("james","Guitar",50);
+        System.out.println(james.instrument);
 
     }
 
     public void testMethod() {
+
+        username = "Lars";
+
+
         int x = 4 + 4 ;
         System.out.println("'value of x : " + x );
 
@@ -27,9 +57,14 @@ public class MainActivity extends AppCompatActivity {
     }
     public int math(int a , int b){
 
+        username = "Kirk";
+
         return a + b;
     }
     public String newMethod(String string){
+
+        username = "Rob";
+
         return string + " is New Method";
     }
 
